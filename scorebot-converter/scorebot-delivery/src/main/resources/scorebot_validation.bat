685ef390fb638df0eef26d3491ad4120c6dcf1eb
Thomas Francart <thomas.francart@sparna.fr>
2022-12-05 22:39:07 +0100

    Update doc and scripts

--- scorebot-converter/scorebot-delivery/src/main/resources/scorebot_validation.bat
+++ scorebot-converter/scorebot-delivery/src/main/resources/scorebot_validation.bat
@@ -7,59 +7,21 @@ echo inputs
 :: Source
 set SHACL_DIR=%DIR_HOME%\shacl
 set RDF_DIR=%DIR_HOME%\output
-
-echo Validate
-set DIR_VALIDATE=%DIR_HOME%\validation-report
-set SHACL_TTL_DIR=%DIR_VALIDATE%\shacl-ttl
-::set DOCUMENTATION_DIR=%DIR_VALIDATE%\documentation
-set REPORT_DIR=%DIR_VALIDATE%\report
-set LOG_DIR=%DIR_VALIDATE%\logs
-
+set DIR_OUTPUT=%DIR_HOME%\output-validation
 
 :: créer les dossiers s'ils n'existent pas.
-mkdir %REPORT_DIR%
-mkdir %SHACL_TTL_DIR%
-::mkdir %DOCUMENTATION_DIR%
-mkdir %LOG_DIR%
+mkdir %DIR_OUTPUT%
 
-:: 
 :: Nettoyer les dosiers des fichiers à valider
-
-del %SHACL_TTL_DIR%\*.ttl
-del %REPORT_DIR%\*.ttl
-del %REPORT_DIR%\*.html
-
-::set SHACL_HTML_DOC=%DOCUMENTATION_DIR%\PHILHARMONIE-General.html
-::set SHACL_PNG=%DOCUMENTATION_DIR%\PHILHARMONIE-General.png
-FOR %%f IN (%SHACL_DIR%\*.xlsx) DO (
-
-	echo ###
-	echo ### Etape 1 - %%f - Conversion en ttl vers %name_file%
-	echo ###
-	
-	java -jar xls2rdf-app-2.1.3-onejar.jar convert -i %%f -o %SHACL_TTL_DIR%\PHILHARMONIE-General.ttl -l en
-)
-
-
-
-	::echo ###
-	::echo ### Etape 2 - %%f - Generation de la doc
-	::echo ###
-
-	::java -jar shacl-play-app-0.5-onejar.jar doc -i %SHACL_TTL_DIR%\PHILHARMONIE-General.ttl -o %SHACL_HTML_DOC% -l en
-	
-	::echo ###
-	::echo ### Etape 3 - %%f - Generation du diagramme
-	::echo ###
-
-	::java -jar shacl-play-app-0.5-onejar.jar draw -i %SHACL_TTL_DIR%\PHILHARMONIE-General.ttl -o %SHACL_PNG%
-
+del %DIR_OUTPUT%\*.ttl
+del %DIR_OUTPUT%\*.html
+del %DIR_OUTPUT%\*.log
 
 echo ###
-echo ### Etape 4 - Validation des data et Generation du rapport
+echo ### Validation des data et Generation du rapport
 echo ###
 
-java -Xmx4048M -jar %DIR_HOME%\shacl-play-app-0.6.2-onejar.jar validate -i %RDF_DIR%\Partitions.rdf -s %SHACL_TTL_DIR%\PHILHARMONIE-General.ttl -o %REPORT_DIR%\PHILHARMONIE-General-report.ttl -o %REPORT_DIR%\PHILHARMONIE-General-report.html > %LOG_DIR%\log_validation.log
+java -Xmx4048M -jar %DIR_HOME%\shacl-play-app-0.6.2-onejar.jar validate -i %RDF_DIR% -s %SHACL_DIR%\scorebot-shacl.ttl -o %DIR_OUTPUT%\scorebot_validation-report.ttl -o %DIR_OUTPUT%\scorebot_validation-report.html > %DIR_OUTPUT%\scorebot_validation-log.log
 
 echo ###
 echo ### Fin
